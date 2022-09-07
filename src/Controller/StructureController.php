7e51a74a1ef5140ad16621a5a6d78df49cf11f2d
Almont michel <michel.almont@gmail.com>
2022-09-07 13:25:10 -0400

    creation page new structure

--- src/Controller/StructureController.php
+++ src/Controller/StructureController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Structure;
+use App\Form\StructureType;
 use App\Repository\StructureRepository;
+use App\Repository\PartenaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +15,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StructureController extends AbstractController
 {
+
+
+     /**
+     * display partenaires
+     *
+     * @param PartenaireRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
     #[Route('/structure/{id}', name: 'structure.index',methods:['GET'])]
     public function index(StructureRepository $repository,PaginatorInterface $paginator,Request $request,int $id): Response
     {
@@ -26,6 +40,35 @@ class StructureController extends AbstractController
 
         return $this->render('pages/structure/index.html.twig', [
             'structures' => $structure,
+            'id'=>$id
+        ]);
+    }
+    #[Route('/structure/new/{id}','structure.new',methods:['GET', 'POST'])]
+    public function new(PartenaireRepository $repository, Request $request, EntityManagerInterface $manager,$id):Response
+    {
+
+        $partenaires =$repository->findOneBy(["id"=>$id]);
+        // dd( $partenaires);
+        $structure = new Structure();
+        $form = $this->createForm(StructureType::class,$structure);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $structure = $form->getData();
+            $structure ->setPartenaire($partenaires);
+            $manager->persist( $structure);
+            $manager->flush();
+            
+            $this->addFlash(
+               'success',
+               'Votre ingrédient à été céer avec succes !'
+            );
+        //  return $this->redirectToRoute('structure.index');
+      
+        }
+        
+        return $this->render('pages/structure/new.html.twig',[
+            'form'=>$form->createView(),
+            'partenaires' => $partenaires
         ]);
     }
 }
---
+++ src/Form/StructureType.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace App\Form;
+use App\Entity\Structure;
+use App\Repository\PartenaireRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class StructureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        
+        $builder
+            ->add('nameStructure', TextType::class,[
+                'attr' =>[
+                    'class' =>'form-control',
+                    'minlength' =>'2',
+                    'maxlength' => '50'
+                ],
+                'required'=> false,
+                'label' => 'Nom de la structure',
+                'label_attr' =>[
+                    'class'=> 'form-label mt-4'
+                ],
+                'constraints' =>[
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('nameResponsable', TextType::class,[
+                'attr' =>[
+                    'class' =>'form-control',
+                    'minlength' =>'2',
+                    'maxlength' => '50'
+                ],
+                'required'=> false,
+                'label' => 'Nom du responsable',
+                'label_attr' =>[
+                    'class'=> 'form-label mt-4'
+                ],
+                'constraints' =>[
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('adresseStructure', TextareaType::class, [
+                'attr' =>[
+                    'class' =>'form-control',
+                    'min' => 1,
+                    'max' => 5
+                ],
+                'label' => 'Adresse de la structure ',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' =>[
+                 new Assert\NotBlank(),
+               
+                ]
+            ])
+            ->add('activeStructure', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'Activation',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersRead', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_read',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersWrite', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_write',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersProduct', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_product',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersPayment', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_payment',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersStistiquesRead', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_stat',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('membersSubscriptionRead', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_subscription',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('paymentSchedulesRead', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_schedules_read',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('paymentShedulesWrite', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_shedule_write',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('paymentDaysRead', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_days_read',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('paymentDaysWrite', CheckboxType::class,[
+                'attr' =>[
+                    'class' =>'form-check-input',
+                ],
+                'required'=> false,
+                'label' => 'members_days_write',
+                'label_attr' =>[
+                    'class'=> 'form-check-label '
+                ],
+                'constraints' =>[
+                    new Assert\NotNull(),
+                   
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'attr'=>[
+                    'class' => 'btn btn-primary mt-4'
+                ],
+                'label' =>'créer un partenaire'
+               ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Structure::class,
+        ]);
+    }
+}
