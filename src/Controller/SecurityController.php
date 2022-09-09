82aa0c1531a3043580de7bdf95d7637684215fad
Almont michel <michel.almont@gmail.com>
2022-09-09 11:54:14 -0400

    formulaire d'enregistrement des comptes admins

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,5 +25,30 @@ class SecurityController extends AbstractController
     #[Route('/deconnexion', 'security.logout')]
     public function logout()
     {
+    }
+
+    #[Route('/inscription','security.registration', methods:['GET','POST'] )]
+    public function registration(Request $request, EntityManagerInterface $manager):Response
+    {
+
+        $user = new User();
+        $user->setRoles(['ROLE_ADMIN']);
+        $form = $this->createForm(RegistrationType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()  && $form->isValid()){
+            $user = $form->getData();
+
+            $this->addFlash(
+                'success',
+                'Votre compte à été crée avec succes !'
+             );
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('security.login');
+        }
+        return $this->render('pages/security/registration.html.twig',[
+            'form'=>$form->createView()
+        ]);
     }
 }
